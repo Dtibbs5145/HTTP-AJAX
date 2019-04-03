da4587415e471b273763fea79afc1698ab54ce1d
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-04-03 17:46:07 -0400

    I have the names showing on the browser

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import FriendsList from './components/FriendsList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      name: '',
+      age: '',
+      email: '',
+  };
+}
+
+componentDidMount() {
+  Axios.get('http://localhost:5000/friends')
+    .then(response => {this.setState({friends: response.data}) })
+    .catch(error => (error));
+}
+
+changeHandler = event => {
+  event.persist();
+  let value = event.target.value;
+
+  this.setState(prevState => ({
+    friend: {
+      ...prevState.friend,
+      [event.target.name]: value
+    }
+  }));
+}
+
+newFriend = e => {
+  e.preventDefault();
+  Axios.post('http://localhost:5000/friends', {
+    name: this.state.name,
+    age: this.state.age,
+    email: this.state.email
+  })
+    .then(response => {this.setState({friends: response.data})})
+    .catch(error => console.log(error));
+  this.setState({
+    name: '',
+    age: 0,
+    email: ''
+  })
+}
+
+render() {
+  return (
+    <div>
+      {this.state.friends.map(friend => (
+        <FriendsList
+        key={friend.id}
+        friend={friend}
+        name={friend.name}
+        age={friend.age}
+        email={friend.email}
+        />
+      ))}
+    </div>
+  )
+}
 }
 
 export default App;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FriendsList = props => {
+    return (
+    <div>
+        <p>{props.friend.name}</p>
+    </div>
+    )
+}
+export default FriendsList;
